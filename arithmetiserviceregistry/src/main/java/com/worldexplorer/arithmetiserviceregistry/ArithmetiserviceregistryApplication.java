f3b3e814f645037e4e4622effcd1d73d33461cb6
manolopzy <manolopeng@gmail.com>
2022-11-14 16:41:25 +0100

    service registry and discovery using eureka

---
+++ arithmetiserviceregistry/src/main/java/com/worldexplorer/arithmetiserviceregistry/ArithmetiserviceregistryApplication.java
@@ -0,0 +1,14 @@
+package com.worldexplorer.arithmetiserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class ArithmetiserviceregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArithmetiserviceregistryApplication.class, args);
+	}
+
+}
